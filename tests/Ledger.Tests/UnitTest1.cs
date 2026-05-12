0bb150f32dd42422539f4ec9e2d6f49051217ac9
Larry Jones <larry.jones@continuous.com>
2026-05-12 18:30:38 -0500

    Revert "Introduce a deliberate failure."
    This reverts commit 823c88c8f6162db20201b34322b43429194950f1.

--- tests/Ledger.Tests/UnitTest1.cs
+++ tests/Ledger.Tests/UnitTest1.cs
@@ -5,6 +5,6 @@ public class UnitTest1
     [Fact]
     public void Test1()
     {
-        Assert.True(false, "Deliberate failure to verify MTP console output on .NET 10.0");
+        Assert.True(true);
     }
 }
