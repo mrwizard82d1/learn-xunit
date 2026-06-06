5957cd235f3971d06a9803321d86299047c2d277
Larry Jones <larry.jones@continuous.com>
2026-06-06 15:32:36 -0500

    Instrument single initialization
    This "side trip" demonstrates that `SeededAccountFixture` available
    through `IClassFixture` is only initialized **once** by printing the
    GUID. The `Console.WriteLine()` code is executed **twice**, but the
    value it prints out in each of those different executions
    is **identical**.

--- tests/Ledger.Tests/AccountQueryTests.cs
+++ tests/Ledger.Tests/AccountQueryTests.cs
@@ -30,6 +30,8 @@ public class AccountQueryTests : IClassFixture<SeededAccountsFixture>
     [Fact]
     public void LifeCycle_PartOne_RecordsFixtureInstanceId()
     {
+        Console.WriteLine($"InstanceId: {_fixture.InstanceId}");
+        
         // No assertion. This test exists merely to demonstrate that the fixture
         // is shared with the `PartTwo` test.
         Assert.NotEqual(Guid.Empty, _fixture.InstanceId);
@@ -38,6 +40,8 @@ public class AccountQueryTests : IClassFixture<SeededAccountsFixture>
     [Fact]
     public void Lifecycle_PartTwo_SeesSameFixtureInstance()
     {
+        Console.WriteLine($"InstanceId: {_fixture.InstanceId}");
+        
         // This test is meaningful only in combination with `PartOne`: both
         // tests received the **same** fixture (and therefore the same
         // `InstanceId`), because `xUnit` instantiated `SeededAccountFixture`
